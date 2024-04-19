9228f93594777a236f0fced2340fc91e7bd0ac6f
Fatma Birel <Fatma Birel@DESKTOP-Q270QVE>
2024-04-19 22:53:05 +0300

    dfsd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,13 +14,13 @@ namespace MyApp
 
             BrandManager brandManager = new BrandManager(new EfBrandDal());
 
-            //foreach(var brand in brandManager.GetAll())
-            //{
-            //    Console.WriteLine(brand.Name);
-            //}
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.Name);
+            }
 
 
-            foreach(var car in carManager.GetCarDetails())
+            foreach (var car in carManager.GetCarDetails())
             {
                 Console.WriteLine(car.Description  + " " + car.BrandName + " " + car.ColorName);
             }
